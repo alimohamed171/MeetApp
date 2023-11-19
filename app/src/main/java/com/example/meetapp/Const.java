1c3de1471adfd130b5d68cf428dcf3855f836af9
ALi <alimohammed1712003@gmail.com>
2023-11-19 13:33:56 +0200

    Initial commit

---
+++ app/src/main/java/com/example/meetapp/Const.java
@@ -0,0 +1,7 @@
+package com.example.meetapp;
+
+public class Const {
+
+    public static long AppID = 1215484200;
+    public static String AppSign = "2a21703f17f37d056fc2e0705f98705dbf765acf0223079b0cd7d45939fbc0e9";
+}
